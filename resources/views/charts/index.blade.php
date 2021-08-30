e1cb0ce3bfdf52b76d4d3a0b44f654313b1b82e5
chimi-pt <prince.tobit@strathmore.edu>
2021-08-31 00:53:59 +0300

    Add chart data

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -6,8 +6,23 @@
     </x-slot>
     
     <div>
+<<<<<<< Updated upstream
     <iframe title="[My Data: test 1]" aria-label="Interactive line chart" id="datawrapper-chart-VElAW" src="https://datawrapper.dwcdn.net/VElAW/1/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height="400"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
 </script>
     </div>    
+=======
+    <iframe src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&time=2020-03-01..latest&facet=none&pickerSort=asc&pickerMetric=location&Metric=Vaccine+doses&Interval=Cumulative&Relative+to+Population=true&Align+outbreaks=false&country=~KEN&hideControls=true" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>
+    </div> 
+    <div class="mt-8">
+    <iframe src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&facet=none&pickerSort=desc&pickerMetric=population&hideControls=true&Metric=People+vaccinated+%28by+dose%29&Interval=Cumulative&Relative+to+Population=true&Align+outbreaks=false&country=~KEN" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>
+    </div> 
+    <div class="mt-8">
+    <iframe src="https://ourworldindata.org/explorers/coronavirus-data-explorer?zoomToSelection=true&facet=none&pickerSort=desc&pickerMetric=population&hideControls=true&Metric=People+vaccinated+%28by+dose%29&Interval=Cumulative&Relative+to+Population=false&Align+outbreaks=false&country=~KEN" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>
+    </div>   
+    <div class="mt-8">
+    <iframe src="https://ourworldindata.org/grapher/daily-covid-vaccination-doses-per-capita?country=~KEN" loading="lazy" style="width: 100%; height: 600px; border: 0px none;"></iframe>
+    </div>
+    
+>>>>>>> Stashed changes
    
 </x-app-layout>
